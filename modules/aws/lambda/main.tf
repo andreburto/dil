53cdf0a7f6f76a99e5008596d8c0bff1ca4443fd
andreburto <tuglyraisin@gmail.com>
2022-09-11 06:38:21 -0500

    Small teaks

--- modules/aws/lambda/main.tf
+++ modules/aws/lambda/main.tf
@@ -7,8 +7,12 @@ terraform {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+locals {
+
+}
+
+resource "aws_iam_role" "iam_for_docker_lambda" {
+  name = "iam_for_${var.function_name}"
 
   assume_role_policy = <<EOF
 {
@@ -29,13 +33,13 @@ EOF
 
 resource "aws_lambda_function" "docker_lambda" {
   function_name = var.function_name
-  role = aws_iam_role.iam_for_lambda.arn
+  role = aws_iam_role.iam_for_docker_lambda.arn
   package_type = "Image"
   image_uri = "${var.repository_url}:latest"
 
   environment {
     variables = {
-      LAMBDA_NAME = "${var.function_name}"
+      LAMBDA_NAME = var.function_name
     }
   }
 }
